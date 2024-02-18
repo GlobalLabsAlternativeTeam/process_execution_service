f701f41f927c5ab00fc0e9dc6b0eb05a2a84bc74
Mauro Parafati <mparafati@fi.uba.ar>
2024-02-18 02:45:54 +0100

    Remove dead code

--- internal/providers/storage/storage_test.go
+++ internal/providers/storage/storage_test.go
@@ -1,43 +1,9 @@
 package storage_test
 
 import (
-	"server/internal/domain"
 	"server/internal/providers/storage"
 	"testing"
 )
-
-type MockStorageProvider struct{}
-
-func (msp *MockStorageProvider) GetTreatments(patientID string) ([]domain.LightTreatment, error) {
-	// Simulate returning a list of light treatments for testing purposes
-	return []domain.LightTreatment{
-		{TreatmentID: "1", TreatmentName: "Treatment 1", TreatmentStatus: "Completed", TreatmentProgress: 100},
-		{TreatmentID: "2", TreatmentName: "Treatment 2", TreatmentStatus: "InProgress", TreatmentProgress: 50},
-	}, nil
-}
-
-func (msp *MockStorageProvider) TreatmentByID(treatmentID string) (domain.Treatment, error) {
-	// Simulate returning a treatment by ID for testing purposes
-	return domain.Treatment{
-		TreatmentID: "1",
-		DoctorID:    "doctorID",
-		PatientID:   "patientID",
-		Status:      "InProgress",
-		StartedAt:   "2024-02-08",
-		FinishedAt:  "",
-		DeletedAt:   "",
-		// Add other necessary fields
-	}, nil
-}
-
-func (msp *MockStorageProvider) GetPatientsByDoctor(doctorID string) ([]string, error) {
-	// Simulate returning a list of patient ids for testing purposes
-	return []string{
-		"asdasd",
-		"xyzxyz",
-		"asd123",
-	}, nil
-}
 
 func TestGetTreatments(t *testing.T) {
 	storageService, err := storage.NewStorage("./test_storage.json")
